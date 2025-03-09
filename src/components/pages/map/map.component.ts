421c72c829cb305f59df7d3a8059a73adb5d7e06
Miquel <abellagarciamiquel@gmail.com>
2025-03-09 13:42:07 +0100

    feat(map): added leaflet map

--- src/components/pages/map/map.component.ts
+++ src/components/pages/map/map.component.ts
@@ -1,11 +1,38 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
     selector: 'app-map',
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
     templateUrl: './map.component.html',
-    styleUrl: './map.component.scss',
+    styleUrls: ['./map.component.scss'],
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+    private map!: L.Map;
+    isOverlayShown = false
 
+    private initMap(): void {
+        this.map = L.map('map', {
+            center: [40.416775, -3.703790], // Center on Madrid
+            zoom: 6, 
+            zoomControl: false,
+        });
+
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?lang=es-ES', {
+            attribution: '© OpenStreetMap contributors',
+            maxZoom: 19,
+            minZoom: 6
+        }).addTo(this.map);
+
+        L.control.zoom({
+            position: 'bottomright',
+        }).addTo(this.map);
+    }
+
+    ngAfterViewInit(): void {
+        this.initMap();
+    }
+
+    toggleOverlay() {
+        this.isOverlayShown = !this.isOverlayShown
+    }
 }
